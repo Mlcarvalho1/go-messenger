8f959660f129d42dc740ec067f51574e6bab3c94
Mlcarvalho1 <manoelliradecarvalho@gmail.com>
2025-02-16 14:37:08 -0300

    adjusted getUser

--- models/user.go
+++ models/user.go
@@ -6,7 +6,12 @@ type User struct {
 	gorm.Model
 	Name      string `json:"name"`
 	Email     string `json:"email" gorm:"unique"`
-	Password  string `json:"-"`
-	Avatar    string `json:"avatar"`
-	FireToken string `json:"fire_token"`
+	Password  string `json:"-"` // Hidden from JSON
+	Avatar    string `json:"avatar,omitempty"`
+	FireToken string `json:"fire_token,omitempty"`
+
+	// Override gorm.Model fields to exclude them from JSON response
+	CreatedAt string `json:"created_at,omitempty" gorm:"-"`
+	UpdatedAt string `json:"updated_at,omitempty" gorm:"-"`
+	DeletedAt string `json:"deleted_at,omitempty" gorm:"-"`
 }
