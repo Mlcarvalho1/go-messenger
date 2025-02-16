26be54b21d98eaa46c05b00de201f17cc1b2cfef
Mend25 <vma3@cin.ufpe.br>
2025-02-16 16:41:12 -0300

    Testes funcionais da função getUser

--- services/user.go
+++ services/user.go
@@ -3,13 +3,18 @@ package services
 import (
 	"go.messenger/database"
 	"go.messenger/models"
+	"gorm.io/gorm"
 )
 
 func GetUser(id int) (models.User, error) {
 	var user models.User
-
-	database.DB.Db.Select("id", "name", "email", "avatar").Where("id = ?", id).First(&user)
-
+	err := database.DB.Db.First(&user, id).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return models.User{}, err
+		}
+		return models.User{}, err
+	}
 	return user, nil
 }
 
---
+++ tests/integration/getUser_test.go
@@ -0,0 +1,55 @@
+package controllers_test
+
+import (
+	"log"
+	"net/http"
+	"net/http/httptest"
+
+	"testing"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
+	"go.messenger/controllers"
+	"go.messenger/database"
+)
+
+func setupApp2() *fiber.App {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+	database.ConnectDb()
+	app := fiber.New()
+	app.Use(logger.New())
+	app.Get("/user/:id", controllers.GetUser)
+	return app
+}
+
+func TestGetUser(t *testing.T) {
+	app := setupApp2()
+
+	t.Run("valid user ID", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/user/1", nil)
+		resp, err := app.Test(req, -1)
+
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+	})
+
+	t.Run("invalid user ID", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/user/invalid", nil)
+		resp, err := app.Test(req, -1)
+
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+	})
+
+	t.Run("user not found", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodGet, "/user/9999", nil)
+		resp, err := app.Test(req, -1)
+
+		assert.NoError(t, err)
+		assert.Equal(t, http.StatusNotFound, resp.StatusCode)
+	})
+}
