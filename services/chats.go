35620cc2b7cde3dd81ba033b8c2c5ea823c2f027
Mlcarvalho1 <manoelliradecarvalho@gmail.com>
2025-03-23 20:40:38 -0300

    included receiver and user id to chat list

--- services/chats.go
+++ services/chats.go
@@ -7,9 +7,15 @@ import (
 
 func GetChatsByUserID(userId int) ([]models.Chat, error) {
 	var chats []models.Chat
-	result := database.DB.Db.Where("user_id = ? OR receiver_id = ?", userId, userId).Find(&chats)
+	result := database.DB.Db.
+		Preload("User").
+		Preload("Receiver").
+		Where("user_id = ? OR receiver_id = ?", userId, userId).
+		Find(&chats)
+
 	if result.Error != nil {
 		return nil, result.Error
 	}
+
 	return chats, nil
 }
