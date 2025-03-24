0f5e32c19895346ebeb8341cba6393f8b8a3cf8b
Mlcarvalho1 <manoelliradecarvalho@gmail.com>
2025-03-23 22:13:20 -0300

    adjusted chatList response

--- services/chats.go
+++ services/chats.go
@@ -17,5 +17,22 @@ func GetChatsByUserID(userId int) ([]models.Chat, error) {
 		return nil, result.Error
 	}
 
+	for i := range chats {
+		chat := &chats[i]
+
+		// If receiverID matches the userId, swap User and Receiver
+		if chat.ReceiverID == uint(userId) {
+			newReceiver := chat.User
+
+			chat.UserID = uint(userId)
+			chat.User = chat.Receiver
+
+			chat.ReceiverID = uint(newReceiver.ID)
+			chat.Receiver = newReceiver
+		} else {
+			chat.UserID = uint(userId)
+		}
+	}
+
 	return chats, nil
 }
